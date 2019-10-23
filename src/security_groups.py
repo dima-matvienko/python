1a0b271f2cd1c4279f47809dbbcbe87e181488d9
Ubuntu <ubuntu@ip-172-31-37-13.eu-central-1.compute.internal>
2019-10-23 02:27:58 +0000

    Added comment for short output

--- src/security_groups.py
+++ src/security_groups.py
@@ -7,9 +7,12 @@ pretty_print = json.dumps(all_security_groups, indent=4)
 
 print(pretty_print)
 
+print('\x1b[6;30;42m' + '\n\n\n\nThis is a short output containing only group names and descriptions\n\n\n\n\n' + '\x1b[0m')
+
 security_groups_short = {}
 
 for security_group in all_security_groups['SecurityGroups']:
    security_groups_short[security_group['GroupName']] = security_group['Description']
 
-print(security_groups_short)
+for groupname, description in security_groups_short.items():
+    print(groupname,description)
