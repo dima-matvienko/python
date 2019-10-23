ed567d7fef276650f0d0e718667a1e1b4a82799f
Ubuntu <ubuntu@ip-172-31-37-13.eu-central-1.compute.internal>
2019-10-23 10:49:04 +0000

    Added - upload to google spreadsheet

--- src/security_groups.py
+++ src/security_groups.py
@@ -1,5 +1,16 @@
+import os
 import json
 import boto3
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+#Defining variables
+csv_path = "./security_groups.csv"
+scope = ['https://www.googleapis.com/auth/drive']
+credentials = "./creds.json"
+spreadsheet_id = "1eQzXpkcnRhCfTu9w9yJosM-x2EBqHQrJWutntfJStQA"
+
+print('\x1b[6;30;42m' + '\n\n\n\nThis is a long output containing complete security group descriptions\n\n\n\n\n' + '\x1b[0m')
 
 client = boto3.client('ec2')
 all_security_groups = client.describe_security_groups()
@@ -17,11 +28,25 @@ for security_group in all_security_groups['SecurityGroups']:
 for groupname, description in security_groups_short.items():
     print(groupname,description)
 
-print('\x1b[6;30;42m' + '\n\n\n\nThis is a short output containing only group names, from and to ports\n\n\n\n\n' + '\x1b[0m')
+print('\x1b[6;30;42m' + '\n\n\n\nWriting csv file with from and to ports\n\n\n\n\n' + '\x1b[0m')
+
+if os.path.exists(csv_path):
+    os.remove(csv_path)
+f = open(csv_path,"a+")
 
 for security_group in all_security_groups['SecurityGroups']:
     for rule in security_group['IpPermissions']:
         try:
-            print('Group name: ' + security_group['GroupName'] + ', FromPort: '  + str(rule['FromPort']) + ', ToPort: ' + str(rule['ToPort']))
+            f.write('Group name: ' + security_group['GroupName'] + ', FromPort: '  + str(rule['FromPort']) + ', ToPort: ' + str(rule['ToPort']) + '\n')
         except KeyError:
             continue
+f.close()
+
+print('\x1b[6;30;42m' + '\n\n\n\nUploading / updating google spreadsheet\n\n\n\n' + '\x1b[0m')
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials, scope)
+gc = gspread.authorize(credentials)
+content = open(csv_path, 'r').read()
+gc.import_csv(spreadsheet_id, content)
+
+print('The link is here: https://docs.google.com/spreadsheets/d/' + spreadsheet_id)
