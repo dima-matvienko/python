8ee4f5f8b201d46a3ee246b19fb302fc656a3e7b
Ubuntu <ubuntu@ip-172-31-37-13.eu-central-1.compute.internal>
2019-10-25 07:00:27 +0000

    major changes

--- src/security_groups.py
+++ src/security_groups.py
@@ -1,7 +1,7 @@
 import os
-import json
 import boto3
 import gspread
+import csv
 from oauth2client.service_account import ServiceAccountCredentials
 
 #Defining variables
@@ -10,43 +10,32 @@ scope = ['https://www.googleapis.com/auth/drive']
 credentials = "./creds.json"
 spreadsheet_id = "1eQzXpkcnRhCfTu9w9yJosM-x2EBqHQrJWutntfJStQA"
 
-print('\x1b[6;30;42m' + '\n\n\n\nThis is a long output containing complete security group descriptions\n\n\n\n\n' + '\x1b[0m')
+#This is for bold font on column headers
 
 client = boto3.client('ec2')
 all_security_groups = client.describe_security_groups()
-pretty_print = json.dumps(all_security_groups, indent=4)
-
-print(pretty_print)
-
-print('\x1b[6;30;42m' + '\n\n\n\nThis is a short output containing only group names and descriptions\n\n\n\n\n' + '\x1b[0m')
-
-security_groups_short = {}
-
-for security_group in all_security_groups['SecurityGroups']:
-   security_groups_short[security_group['GroupName']] = security_group['Description']
-
-for groupname, description in security_groups_short.items():
-    print(groupname,description)
-
-print('\x1b[6;30;42m' + '\n\n\n\nWriting csv file with from and to ports\n\n\n\n\n' + '\x1b[0m')
 
 if os.path.exists(csv_path):
     os.remove(csv_path)
-f = open(csv_path,"a+")
 
-for security_group in all_security_groups['SecurityGroups']:
-    for rule in security_group['IpPermissions']:
-        try:
-            f.write('Group name: ' + security_group['GroupName'] + ', FromPort: '  + str(rule['FromPort']) + ', ToPort: ' + str(rule['ToPort']) + ',IPs: ' + str(rule['IpRanges']).replace(',',' ') + ', UserIdGroupPairs ' + str(rule['UserIdGroupPairs']).replace(',',' ') + '\n')
-        except KeyError as error:
-            if (str(error)) == "'FromPort'":
+with open(csv_path, 'w') as csv_file:
+    csvwriter = csv.writer(csv_file, delimiter=',')
+    csvwriter.writerow(['Group Name', 'Group ID', 'Port Range', 'Source'])
+    for security_group in all_security_groups['SecurityGroups']:
+        for rule in security_group['IpPermissions']:
+            for CidrIp in rule['IpRanges']:
                 try:
-                    f.write('Group name: ' + security_group['GroupName'] + ', FromPort: '  + 'All' + ', ToPort: ' + str(rule['ToPort']) + ',IPs:' + str(rule['IpRanges']).replace(',',' ') + ', UserIdGroupPairs ' +  str(rule['UserIdGroupPairs']).replace(',',' ') + '\n')
-                except KeyError as error2:
-                    if (str(error2)) == "'ToPort'":
-                        f.write('Group name: ' + security_group['GroupName'] + ', FromPort: '  + 'All' + ', ToPort: ' + 'All' + ',IPs:' + str(rule['IpRanges']).replace(',',' ') +  ', UserIdGroupPairs ' + str(rule['UserIdGroupPairs']).replace(',',' ') + '\n')
-
-f.close()
+                    if rule['FromPort'] == rule['ToPort']:
+                        csvwriter.writerow([security_group['GroupName'], security_group['GroupId'], str(rule['FromPort']), CidrIp['CidrIp']])
+                    else:
+                        csvwriter.writerow([security_group['GroupName'], security_group['GroupId'], str(rule['FromPort']) + '-' + str(rule['ToPort']), CidrIp['CidrIp']])
+                except KeyError as error:
+                    if (str(error)) == "'FromPort'":
+                        try:
+                            csvwriter.writerow([security_group['GroupName'], security_group['GroupId'], 'All' + '-' + str(rule['ToPort']), CidrIp['CidrIp']])
+                        except KeyError as error2:
+                            if (str(error2)) == "'ToPort'":
+                                csvwriter.writerow([security_group['GroupName'], security_group['GroupId'], 'All', CidrIp['CidrIp']])
 
 print('\x1b[6;30;42m' + '\n\n\n\nUploading / updating google spreadsheet\n\n\n\n' + '\x1b[0m')
 
